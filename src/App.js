3b49ae77d8fc1ddda0d3e2c26eb4439ba809e925
Semyon <125258352+SSUHOY@users.noreply.github.com>
2023-11-29 13:13:05 +0700

    install eslint

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { GlobalStyle } from "./components/styles/global/Global.styles";
 import Main from "./pages/mainPage";
+import React from "react";
 
 function App() {
   return (
