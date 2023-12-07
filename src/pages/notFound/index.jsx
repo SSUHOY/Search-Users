aadfab07ef00c91514b9875028f40c05e0d8b4af
Semyon <125258352+SSUHOY@users.noreply.github.com>
2023-12-07 13:32:38 +0700

    creates 404 page layout and adds back to '/' btn

--- src/pages/notFound/index.jsx
+++ src/pages/notFound/index.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 
+import { SearchTitle } from "../mainPage/mainPage.styles";
+import { Container404, ErrorCode, ToMainButton } from "./notFound.styles";
+import { Link } from "react-router-dom";
+
 export const NotFound = () => {
-  return <div>Страница не найдена</div>;
+  return (
+    <Container404>
+      <ErrorCode>404</ErrorCode>
+      <SearchTitle>Oops..Page not found</SearchTitle>
+      <Link to="/">
+        <ToMainButton>Back to main</ToMainButton>
+      </Link>
+    </Container404>
+  );
 };
